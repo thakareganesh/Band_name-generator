3997fa95c71bd1bfc32eec6084e5024b16cf2463
Ganesh Thakare <65540177+GannuThakare@users.noreply.github.com>
2023-10-27 05:51:04 +0530

    Create band-gen.py

---
+++ band-gen.py
@@ -0,0 +1,7 @@
+
+print("What is your good name, please enter it: ")
+print("Hello " + input() + "!" +" , Welcome to Band Name Generator!")
+city_name = input("Which city do you grow up in? \n")
+pet_name = input("What is your pet name? \n")
+# print("I am suggesting you, Your bands name should be " +  city_name + " " + pet_name)
+print("I am suggesting you, Your bands name should be " + "'" + city_name + " " + pet_name + "'.")
